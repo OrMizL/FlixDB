8f739a11db2d9c84379a8a3fe9bd9c6c1e70a73d
OrMizL <or0mizrahi@gmail.com>
2022-01-25 20:15:31 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const fetchMoviesHandler = async () => {
+    const results = await fetch(
+      'https://api.themoviedb.org/3/discover/movie?api_key=85b6cb8ed71b2f68b11d9bea9f3fa027'
+    );
+    const data = await results.json();
+    console.log(data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={fetchMoviesHandler}>Retrieve Movies</button>
     </div>
   );
 }
