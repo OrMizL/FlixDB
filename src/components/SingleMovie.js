2bfff1496c969b95964c7d368c562e12c0bc2a2a
OrMizL <or0mizrahi@gmail.com>
2022-02-03 21:30:10 +0200

    added single movie page

--- src/components/SingleMovie.js
+++ src/components/SingleMovie.js
@@ -1,7 +1,50 @@
 import classes from './SingleMovie.module.css';
+import { BsStarFill, BsCalendarFill } from 'react-icons/bs';
+import { FaCrown, FaFilm, FaVoteYea } from 'react-icons/fa';
 
 const SingleMovie = (props) => {
-  return <div className={classes.title}>{props.title}</div>;
+  return (
+    <div className={classes.all}>
+      <div className={classes.title}>
+        <h1>{props.movieDetails.title}</h1>
+      </div>
+      <div className={classes.info}>
+        <div className={classes.media}>
+          {/* <div className={classes.video}>
+          {/* <Youtube videoId={movieVideoId}></Youtube> */}
+          {/* </div> */}
+          <div className={classes.poster}>
+            <img src={props.movieDetails.poster} alt='poster'></img>
+          </div>
+        </div>
+        <div className={classes.details}>
+          <div className={classes.overview}>{props.movieDetails.overview}</div>
+          <div className={classes.first}>
+            <div>
+              <BsStarFill className={classes.icons} />
+              {props.movieDetails.rating}
+            </div>
+            <div>
+              <FaVoteYea className={classes.icons} />
+              {props.movieDetails.votes}
+            </div>
+            <div>
+              <FaCrown className={classes.icons} />
+              {props.movieDetails.popularity}
+            </div>
+            <div>
+              <FaFilm className={classes.icons} />
+              {props.movieDetails.runtime}min
+            </div>
+            <div>
+              <BsCalendarFill className={classes.icons} />
+              {props.movieDetails.releasedOn}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SingleMovie;
--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,29 +1,49 @@
 import { useParams, Outlet } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import SingleMovie from '../components/SingleMovie';
 import configData from '../config.json';
 
 const MovieDetail = () => {
-  const [movieDetails, setMovieDetails] = useState([]);
+  const [movieDetails, setMovieDetails] = useState({
+    id: '',
+    title: '',
+    poster: '',
+    overview: '',
+    rating: null,
+    popularity: null,
+    runtime: null,
+    releasedOn: '',
+    votes: null,
+  });
 
   const params = useParams();
   const { movieId } = params;
 
-  const fetchMovieDetails = async () => {
+  const fetchMovieDetails = useCallback(async () => {
     const results = await fetch(
       `https://api.themoviedb.org/3/movie/${movieId}?api_key=${configData.API_KEY}`
     );
     const data = await results.json();
-    setMovieDetails(data);
-  };
+    setMovieDetails({
+      id: data.id,
+      title: data.title,
+      poster: `https://image.tmdb.org/t/p/w300/${data.poster_path}`,
+      overview: data.overview,
+      rating: data.vote_average,
+      popularity: data.popularity,
+      runtime: data.runtime,
+      releasedOn: data.release_date,
+      votes: data.vote_count,
+    });
+  }, [movieId]);
 
   useEffect(() => {
     fetchMovieDetails();
-  });
+  }, [fetchMovieDetails]);
 
   return (
     <>
-      <SingleMovie title={movieDetails.title} />
+      <SingleMovie movieDetails={movieDetails} />
       <Outlet />
     </>
   );
